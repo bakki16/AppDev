5f66f8b2b099816fb5834410de960dd612734227
bakkesh.subedhar <bakkesh.subedhar@host.docker.internal>
2023-07-26 12:02:09 +0530

    Git Test demo commit

---
+++ gittest/src/main/java/training/iqgateway/gittest/App.java
@@ -0,0 +1,14 @@
+package training.iqgateway.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Welcome to git");
+        System.out.println( "Hello World!" );
+    }
+}
